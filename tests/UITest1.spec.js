b860d06fc1ff68f241eb1a5c29d11bc45d53111f
naveen9337 <devrohnav@gmail.com>
2025-06-13 18:33:25 +0530

    Add into llatest

--- tests/UITest1.spec.js
+++ tests/UITest1.spec.js
@@ -1,5 +1,6 @@
 import {test,expect} from '@playwright/test';
-import { BasePage } from '../src/pom/BasePage';
+console.log('Test for Git 1');
+console.log('Test for Git 1');
 
 //Browser(Fixture) - glabally available to each and every playwright test
 test('Browser context declaration first Test',async ({browser})=>{
